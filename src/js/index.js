605f13b7587759eab2ca294fb806f5d241094498
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-09-19 13:37:18 +0100

    updated package.json to include @babel/core and @babel/preset-env, also added blog cards and record section as well as back button for header-2

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,15 @@ window.onload = function () {
   //console.log(this)
 }
 import '../sass/main.scss';
+const aboutBtn = document.getElementById('header__about--link');
+const aboutBackBtn = document.getElementById('header-2--backbtn');
+const aboutCheckBox = document.getElementById('header--checkbox');
+aboutBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  aboutCheckBox.checked = true;
+})
+
+aboutBackBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  aboutCheckBox.checked = false;
+})
