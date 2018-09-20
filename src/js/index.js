fe7ba10aaf3786dbbf8925a33d604ea423b4a4b3
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-09-20 15:23:04 +0100

    added contact form and overlay functionality

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,14 @@ window.onload = function () {
   //console.log(this)
 }
 import '../sass/main.scss';
+const overlay = document.getElementById('overlay');
 const aboutBtn = document.getElementById('header__about--link');
+const contactBtn = document.getElementById('header__contact--link');
 const aboutBackBtn = document.getElementById('header-2--backbtn');
+const overLayCheckBox = document.getElementById('overlay--control');
+const contactSection = document.getElementById('contact--section');
 const aboutCheckBox = document.getElementById('header--checkbox');
+
 aboutBtn.addEventListener('click', function (e) {
   e.preventDefault();
   aboutCheckBox.checked = true;
@@ -13,4 +18,17 @@ aboutBtn.addEventListener('click', function (e) {
 aboutBackBtn.addEventListener('click', function (e) {
   e.preventDefault();
   aboutCheckBox.checked = false;
+})
+
+contactBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  overLayCheckBox.checked = true;
+})
+
+contactSection.addEventListener('click', function () {
+  overLayCheckBox.checked = true;
+});
+
+overlay.addEventListener('click', function () {
+  overLayCheckBox.checked = false;
 })
